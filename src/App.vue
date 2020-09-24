58b31dc6581cbc4f93cbdbf1af70175d78355216
Emmanuel Neni <emmanuelneni@gmail.com>
2020-09-24 19:07:18 +0300

    instaling dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>{{ title }}</h1>
+    <b-icon-plus></b-icon-plus><span>Add appointment</span>
+    <div class="appointments" v-for="(appointment, index) in appointments" :key="index">
+      <div class="appointments-list"><h3>{{ appointment.petName }}</h3>
+      <p>{{ appointment.aptNotes }}</p></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {
+      title: "Appointment List",
+      appointments: [
+        {
+          petName: "Spot",
+          petOwner: "Constance Smith",
+          aptDate: "2017-07-24 08:30",
+          aptNotes: "This German Shepherd is having some back pain",
+        },
+        {
+          petName: "Goldie",
+          petOwner: "Barot Bellingham",
+          aptDate: "2017-07-22 15:50",
+          aptNotes: "This Goldfish has some weird spots in the belly",
+        },
+        {
+          petName: "Mitten",
+          petOwner: "Hillary Goldwyn",
+          aptDate: "2017-07-21 9:15",
+          aptNotes: "Cat has excessive hairballs",
+        },
+        {
+          petName: "Buffy",
+          petOwner: "Hassum Harrod",
+          aptDate: "2017-07-20 15:30",
+          aptNotes:
+            "This Chihuahua has not eaten for three days and is lethargic",
+        },
+      ],
+    };
+  },
+  methods: {
+    getAppointments(){
+      this.$axios.get("https://gist.githubusercontent.com/planetoftheweb/46426d47f21f2c9245bbe23f0fb834b5/raw/afae0adf97472893288ac5cde69e7bbb770793ed/appointments.json",)
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
@@ -21,8 +60,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>

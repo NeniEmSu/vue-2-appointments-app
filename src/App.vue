4862829c6fd5589c9d2cf3e4b56b3bba84185b6c
Emmanuel Neni <emmanuelneni@gmail.com>
2020-09-25 01:17:41 +0300

    added search and filter fucntionality

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,89 @@
   <div id="app" class="container">
     <h1>{{ title }}</h1>
     <AddAppointment @add="addApt" />
-    <AppointmentList :appointments="appointments" :loading="loading" :errror="error" @remove="removeRecord" @edit="editItem"/>    
+    <SearchAppointments
+      :myKey="filterKey"
+      :myDir="filterDir"
+      @searchRecords="searchApt"
+      @update-key="updateKey"
+      @update-dir="updateDir"
+    />
+    <AppointmentList
+      :appointments="filteredApt"
+      :loading="loading"
+      :errror="error"
+      @remove="removeRecord"
+      @edit="editItem"
+    />
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import _ from "lodash";
 import AppointmentList from "./components/AppointmentList.vue";
-import AddAppointment from './components/AddAppointment';
+import AddAppointment from "./components/AddAppointment";
+import SearchAppointments from "./components/SearchAppointments";
 
 export default {
   name: "App",
-  components: { AppointmentList, AddAppointment },
-   data() {
+  components: { AppointmentList, AddAppointment, SearchAppointments },
+  data() {
     return {
       title: "Appointment List",
       appointments: [],
+      aptIndex: 0,
       loading: false,
       error: "",
-      aptIndex: 0,
+      searchTerms: "",
+      filterKey: "petName",
+      filterDir: "asc",
     };
   },
   mounted() {
     this.getAppointments();
   },
-  methods: {
-    addApt(formData){
-      formData.aptId = this.aptIndex
-      this.aptIndex++
-      this.appointments.push(formData)
+  computed: {
+    searchedApt() {
+      return this.appointments.filter((item) => {
+        return (
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        );
+      });
     },
-    editItem(id, field, text){
-        const itemIndex = this.appointments.findIndex(item => item.aptId === id)
-        this.appointments[itemIndex][field ]= text        
+
+    filteredApt() {
+      return _.orderBy(
+        this.searchedApt,
+        (item) => {
+          return item[this.filterKey].toLowerCase();
+        },
+        this.filterDir
+      );
+    },
+  },
+  methods: {
+    addApt(formData) {
+      formData.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(formData);
+    },
+    editItem(id, field, text) {
+      const itemIndex = this.appointments.findIndex(
+        (item) => item.aptId === id
+      );
+      this.appointments[itemIndex][field] = text;
+    },
+    searchApt(terms) {
+      this.searchTerms = terms;
+    },
+    updateKey(value) {
+      this.filterKey = value;
+    },
+    updateDir(value) {
+      this.filterDir = value;
     },
     removeRecord(item) {
       this.appointments = this.appointments.filter((elm) => elm !== item);
---
+++ src/components/SearchAppointments.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="search-appointments row justify-content-center my-4">
+    <div class="col-md-6">
+      <div class="input-group">
+        <input
+          id="SearchApts"
+          type="text"
+          class="form-control"
+          aria-label="Search Appointments"
+          v-model="searchTerm"
+        />
+        <div class="input-group-append">
+          <b-dropdown
+            id="dropdown-1"
+            text="Sort by:"
+            class="sort-menu dropdown-menu-right"
+          >
+            <b-dropdown-item @click="$emit('update-key', 'petName')"
+              >Pet Name
+              <b-icon-check
+                v-if="myKey === 'petName'"
+                class="ml-2"
+              ></b-icon-check
+            ></b-dropdown-item>
+            <b-dropdown-item @click="$emit('update-key', 'aptDate')"
+              >Date
+              <b-icon-check
+                v-if="myKey === 'aptDate'"
+                class="ml-2"
+              ></b-icon-check
+            ></b-dropdown-item>
+            <b-dropdown-item @click="$emit('update-key', 'petOwner')"
+              >Owner
+              <b-icon-check
+                v-if="myKey === 'petOwner'"
+                class="ml-2"
+              ></b-icon-check
+            ></b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item @click="$emit('update-dir', 'asc')"
+              >Asc
+              <b-icon-check v-if="myDir === 'asc'" class="ml-2"></b-icon-check
+            ></b-dropdown-item>
+            <b-dropdown-item @click="$emit('update-dir', 'desc')"
+              >Desc
+              <b-icon-check v-if="myDir === 'desc'" class="ml-2"></b-icon-check
+            ></b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchAppointments",
+  props: {
+    myKey: {
+      default: "petName",
+      type: String,
+    },
+    myDir: {
+      default: "asc",
+      type: String,
+    },
+  },
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
+  watch: {
+    searchTerm() {
+      this.$emit("searchRecords", this.searchTerm);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
